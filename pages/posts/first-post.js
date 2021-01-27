2a6c8a160ce2e8f533eaffbd77421502ea30efeb
Kryštof Svatoň <Svatykr@gmail.com>
2021-01-27 22:07:49 +0100

    Wonders of next Js and Styled components

---
+++ pages/posts/first-post.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Head from "next/head";
+import styled from 'styled-components';
+
+export default function FirstPost() {
+  return (
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+        <WelcomeWrapper>
+            <WelcomeText className="blue">Welcome!</WelcomeText>
+        </WelcomeWrapper>
+      <Link href='/'>
+        <a>Back home</a>
+      </Link>
+    </div>
+  );
+}
+
+
+const WelcomeWrapper = styled.div`
+
+    h1 {
+      color: yellow;
+    }
+
+`
+const WelcomeText = styled.h1`
+  
+`
